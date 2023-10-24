f01439337245add8f1bf6b29e6e4910e95b68d58
Tukkis <54272662+Tukkis@users.noreply.github.com>
2023-10-24 09:40:22 +0300

    add mui datepicker

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import Todotable from './Todotable';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import moment from 'moment';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 
 function Todolist() {
 
     const [ desc, setDesc ] = useState({description:'', date:''});
     const [ todos, setTodos ] = useState([]);
+    const [value, setValue] = useState(null);
   
     const handleDescriptionInput = (e) => {
         setDesc({...desc, description: e.target.value})
     }
   
-    const handleDateInput = (e) => {
-        setDesc({...desc, date: e.target.value})
-    }
-  
     const addTodo = (e) => {
+      const newDate = `${moment(value).date()}.${moment(value).month()}.${moment(value).year()}`
+      const copy = desc;
+      copy.date = newDate
       e.preventDefault()
-      setTodos([...todos, desc])
+      setTodos([...todos, copy])
       setDesc({description:'', date:''})
     }
   
@@ -29,7 +35,11 @@ function Todolist() {
       <div className="App">
         <form onSubmit={addTodo }> 
           <input type="text" value={desc.description} onChange={handleDescriptionInput} />
-          <input type="date" value={desc.date} onChange={handleDateInput} />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={['DatePicker']}>
+              <DatePicker defaultValue={dayjs('2023-10-24')} value={value} onChange={(newValue) => setValue(newValue)} />
+            </DemoContainer>
+          </LocalizationProvider>
           <input type="submit" value="Add"/>
         </form>
         <Todotable deleteTodo={deleteTodo} todos={todos} />
