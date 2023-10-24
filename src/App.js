bec76db556983ed2177dff59d71237cbc36f4e3c
Tukkis <54272662+Tukkis@users.noreply.github.com>
2023-10-24 09:52:34 +0300

    add tab menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Todolist from './Todolist';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 
 function App() {
+
+  const [tab, setTab] = useState('Home');
+
+  const handleChange = (event, value) => {
+      setTab(value);
+    };
+
   return (
     <div className="App">
-      <Todolist />
+      <Tabs value={tab} onChange={handleChange}>
+        <Tab value="Home" label="Home" />
+        <Tab value="Todolist" label="Todolist" />
+      </Tabs>
+      {tab === 'Home' && <div>Welcome!</div>}
+      {tab === 'Todolist' && <Todolist />}
     </div>
   );
 }
